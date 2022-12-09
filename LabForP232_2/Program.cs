cd6bc4af1b01cbaac9b6ae4f6ae2c9948a0ffac7
vusal-imanoff <vusalmi@code.edu.az>
2022-12-09 17:50:55 +0400

    done

---
+++ LabForP232_2/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+namespace LabForP232_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task - 1
+            //Verilmiş sözün ilk hərifinin böyük A olub olmamasını yoxlayan proqram yazın
+
+
+            //Console.WriteLine("Sozu daxil edin");
+            //var word = Console.ReadLine();
+
+            //if (word[0] == 'A')
+            //{
+            //    Console.WriteLine("Birinci herif A ya beraberdoir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Birinci herif A ya beraber deyil");
+            //}
+
+            //Task-2
+            //Verilmiş sözdə b hərfi olub olmamasını yoxlayan proqram yazın
+
+            //Console.WriteLine("Adinizi daxil edin:");
+            //var name=Console.ReadLine();
+            //bool hasB=false;
+
+            //for(int i = 0; i < name.Length; i++)
+            //{
+            //    if (name[i]== 'b' || name[i] == 'B')
+            //    {
+            //        hasB=true;
+            //        break;
+            //    }
+            //}
+
+            //if(hasB == true)
+            //{
+            //    Console.WriteLine("B herfi var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("B herfi yoxdur");
+            //}
+
+            //Task - 3
+            //İsdifadəçi Ədəd Daxil Edir. Daxil Edilən Ədədin Müsbət Olmalıdır.Daxil Edilən Ədədin
+            //Həm 7 - ə Həm Də 3 - ə Bölünüb Bölünməməsini Tapan proqram yazın
+
+            //Console.WriteLine();
+            //var number = Console.ReadLine();
+            //var num=Convert.ToInt32(number);
+
+            //if(num>0 )
+            //{
+            //    if (num % 3 == 0 && num % 7 == 0)
+            //    {
+            //        Console.WriteLine("eded bolunur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("eded bolunmur");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("menfidir");
+            //}
+
+
+
+            //Task - 4
+            //Verilmiş 4 ədəd içərisindən neçəsinin 3 - ə tam bölündüyünü tapan proqram yazın
+
+            //Console.WriteLine("ededleri daxil edin");
+            //var number1 = Console.ReadLine();
+            //var num1 = Convert.ToInt32(number1);
+            //var number2 = Console.ReadLine();
+            //var num2 = Convert.ToInt32(number2);
+            //var number3 = Console.ReadLine();
+            //var num3 = Convert.ToInt32(number3);
+            //var number4 = Console.ReadLine();
+            //var num4 = Convert.ToInt32(number4);
+            //var counter = 0;
+            //if (num1 % 3 == 0)
+            //{
+
+            //    counter++;
+            //}
+            //if (num2 % 3 == 0)
+            //{
+            //    counter++;
+            //}
+            //if (num3 % 3 == 0)
+            //{
+            //    counter++;
+            //}
+            //if (num4 % 3 == 0)
+            //{
+            //    counter++;
+            //}
+            //Console.WriteLine(counter);
+            //if (counter == 0)
+            //{
+            //    Console.WriteLine("bolunmur");
+            //}
+
+
+            //Task - 5
+            //İstənilən 4 ədədin müsbətlərinin hasilini və mənfilərinin cəmini tapan
+            //proqram yazın
+
+
+
+
+            //Console.WriteLine("ededleri daxil edin");
+            //var number1 = Console.ReadLine();
+            //var num1 = Convert.ToInt32(number1);
+            //var number2 = Console.ReadLine();
+            //var num2 = Convert.ToInt32(number2);
+            //var number3 = Console.ReadLine();
+            //var num3 = Convert.ToInt32(number3);
+            //var number4 = Console.ReadLine();
+            //var num4 = Convert.ToInt32(number4);
+            //var sum = 0;
+            //var hasil = 1;
+
+
+            //if (num1 > 0)
+            //{
+
+            //    hasil*=num1;
+            //}
+            //else 
+            //{
+            //    sum += num1;
+
+            //}
+
+            //if (num2 > 0)
+            //{
+
+            //    hasil *= num2;
+            //}
+            //else
+            //{
+            //    sum += num2;
+
+            //}
+            //if (num3 > 0)
+            //{
+
+            //    hasil *= num3;
+            //}
+            //else
+            //{
+            //    sum += num3;
+
+            //}
+
+            //if (num4 > 0)
+            //{
+
+            //    hasil *= num4;
+            //}
+            //else
+            //{
+            //    sum += num4;
+
+            //}
+            //Console.WriteLine(hasil);
+            //Console.WriteLine(sum);
+
+            //Task-6
+            //Verilmiş arraydaki ədələrin cütlərinin cəmini və təklərinin hasilini tapan proqram yazın
+
+            //int[] array = new int[] { 34, 45, 66, 78, 23, 14 };
+            //var sum = 0;
+            //var multiply = 1;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (array[i] % 2 == 0)
+            //        sum += array[i];
+            //    else
+            //        multiply *= array[i];
+
+            //}
+            //Console.WriteLine(sum + " " + multiply);
+        }
+    }
+}
